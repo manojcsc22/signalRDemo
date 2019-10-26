e19a6786dfed0900b3ceb97ffd84bd7abced9ba7
manojcsc22 <manojcsc22@gmail.com>
2019-10-26 16:34:39 +0530

    New Project

---
+++ HitCounterSignal/HitCounter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HitCounterSignal
+{
+    [HubName("hitCounter")]
+    public class HitCounter : Hub
+    {
+        static string _hitcount = "<br>I am so sorry my dear bestie Deepika " + 0 + "<br>";
+        static int _count = 0;
+        public void RecordHit()
+        {
+            _count += 1;
+            _hitcount += "<br>I am so sorry my dear bestie Deepika " + _count + "<br>";
+            Clients.All.onRecordHit(_hitcount);
+        }
+    }
+}
---
+++ HitCounterSignal/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HitCounterSignal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HitCounterSignal")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84d3617a-cb4e-4c5f-b8e3-585e80fb9895")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
